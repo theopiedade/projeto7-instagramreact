dcd9310d3b59783f4235b8c638e9edd5e8a08670
Theo <theo.piedade@gmail.com>
2023-07-01 17:48:14 -0300

    Modules init created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Body from "./Body"
+import NavBar from "./NavBar"
+
 export default function App () {
     return (
-        <>
-        
-        </>
+       <Body>
+            <NavBar/>
+            
+       </Body>
     )
 }
---
+++ src/Body.jsx
@@ -0,0 +1,5 @@
+export default funcion Body() {
+    returm (
+
+    );
+}
